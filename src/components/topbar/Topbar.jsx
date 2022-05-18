7b24ceeb2acc8939ff60546bd1685ee7526e2661
Fatuma <sfatex@gmail.com>
2022-05-17 22:11:50 -0400

    Add ids for components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./topbar.scss";
 export default function Topbar() {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <a href="#intro">Intro</a>
+        <div className="right"></div>
+        </div>;
+    </div>
+  );
 }
