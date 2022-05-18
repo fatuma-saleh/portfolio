cb6c062c2fdb56072a4c84e5675d54e42c0b4a48
Fatuma <sfatex@gmail.com>
2022-05-17 23:40:26 -0400

    Install more materaila ui dependencies

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./topbar.scss";
+import { Person,Mail } from '@mui/icons-material';
 export default function Topbar() {
   return (
     <div className="topbar">
@@ -9,12 +10,16 @@ export default function Topbar() {
             Intro
           </a>
           <div className="itemContainer">
-            
+            <Person  className="icon"/>
+            <span>+1-647-293-8566</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>sfatex@gmail.com</span>
           </div>
         </div>
         <div className="right"></div>
       </div>
-      
     </div>
   );
 }
