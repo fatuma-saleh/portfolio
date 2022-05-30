633af5d7630855a9897b81376fe86d759d731755
Fatuma <sfatex@gmail.com>
2022-05-30 13:46:54 -0400

    Style and divide Intro Component in  to two halves

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import "./intro.scss"
+import React from "react";
+import "./intro.scss";
 
 export default function Intro() {
   return (
-    <div className='intro' id="intro">
-      
+    <div className="intro" id="intro">
+      <div className="left"></div>
+      <div className="right"></div>
     </div>
-  )
+  );
 }
