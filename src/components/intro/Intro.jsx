be7b097e61c8a51e7af68e950cfd64f9dfdc3d3b
Fatuma <sfatex@gmail.com>
2022-06-01 23:10:14 -0400

    change show curser to true

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import "./intro.scss";
 import { init } from "ityped";
 
 export default function Intro() {
   const textRef = useRef();
 
-  useEffect(() =>{
-   init(textRef.current,{
-     showCursor:false,
-     strings:["Back-End", "Front-End", "Full-Stack"],
-   })
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
 
-  },[])
+      strings: ["Back-End", "Front-End", "Full-Stack"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -23,13 +25,14 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There! I'm</h2>
           <h1>Fatuma Saleh</h1>
-          <h3>Web Developer<span ref={textRef}> </span></h3>
+          <h3>
+            Web Developer<span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portfolio">
-        <img src="docs/arrow.png" alt="" />
+          <img src="docs/arrow.png" alt="" />
         </a>
       </div>
-
     </div>
   );
 }
