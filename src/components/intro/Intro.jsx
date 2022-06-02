a03e6cd55f4516aee10b64668a11d83d5eca076f
Fatuma <sfatex@gmail.com>
2022-06-01 22:47:51 -0400

    Add useRef

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef} from "react";
 import "./intro.scss";
 import { init } from "ityped";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() =>{
+   init(textRef.current,{
+     showCursor:false,
+     strings:["Back-End", "Front-End", "Full-Stack"],
+   })
+
+  },[])
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +23,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There! I'm</h2>
           <h1>Fatuma Saleh</h1>
-          <h3>Full Stack Web Developer</h3>
+          <h3>Web Developer<span ref={textRef}> </span></h3>
         </div>
         <a href="#portfolio">
         <img src="docs/arrow.png" alt="" />
