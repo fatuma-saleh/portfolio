15f78b7af3bf447544ac196d82c80bdd820ae912
Fatuma <sfatex@gmail.com>
2022-06-20 14:23:34 -0400

    Add layout for right part of works component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import "./works.scss"
+import React from "react";
+import "./works.scss";
 export default function Works() {
   return (
-    <div className='works' id="works">
+    <div className="works" id="works">
       <div className="slider">
         <div className="container">
           <div className="item">
@@ -11,15 +11,23 @@ export default function Works() {
                 <div className="imgContainer">
                   <img src="docs/iCard.png" alt="" />
                 </div>
-                <h2>Title</h2>
-                <p>Description</p>
+                <h2>iCard</h2>
+                <p>
+                  iCard is a single-page application (SPA) that allows users to
+                  create digital business cards, and share it to others through
+                  an auto generated QR code. The front end of this project is
+                  built with React and makes requests to an API to fetch and
+                  store appointment data from a database.
+                </p>
                 <span>Projects</span>
               </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+              <img src="docs/iCard.png" alt="" />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
