d5aba894f4fcc87121ea9a58bda44ec19f7bd3e8
Fatuma <sfatex@gmail.com>
2022-06-20 12:24:17 -0400

    Add left and right slider for porojects

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -6,7 +6,16 @@ export default function Works() {
       <div className="slider">
         <div className="container">
           <div className="item">
-            <div className="left"></div>
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="docs/iCard.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>Description</p>
+                <span>Projects</span>
+              </div>
+            </div>
             <div className="right"></div>
           </div>
         </div>
