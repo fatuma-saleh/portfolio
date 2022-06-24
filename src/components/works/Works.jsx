15bb9eb37188f48c179c81ebaab25fcf53344b54
Fatuma <sfatex@gmail.com>
2022-06-24 12:28:51 -0400

    Add arrow back and foreward functions

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,37 +1,49 @@
 import React, { useState } from "react";
 import "./works.scss";
-const data = require("../../data")
+const data = require("../../data");
 export default function Works() {
-  const [currentSlide,setCurrrentSlide] = useState(0)
+  const [currentSlide, setCurrrentSlide] = useState(0);
+  const handleclick = (direction) =>{
+direction==="left"? setCurrrentSlide(currentSlide > 0 ? currentSlide-1:3):setCurrrentSlide(currentSlide<3?currentSlide+1:0)
+  }
   return (
     <div className="works" id="works">
-      <div className="slider" style={{transform:`translateX(-${currentSlide *100}vw)`}}>
-        {data.map(d=>(
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
           <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src={d.icon} alt="" />
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>{d.title}</h2>
-                <p>
-                 {d.desc }
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
               </div>
             </div>
-            <div className="right">
-              <img src={d.img} alt="" />
-            </div>
           </div>
-        </div>
         ))}
-        
       </div>
-      <img src="docs/arrow.png"className="arrow left" alt="" />
-      <img src="docs/arrow.png"className="arrow right" alt="" />
-
+      <img
+        src="docs/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleclick("left")}
+      />
+      <img
+        src="docs/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleclick("right")}
+      />
     </div>
   );
 }
