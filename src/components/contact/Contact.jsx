e0f1fabd33673e369923b41b77ea7a01d27e2f80
Fatuma <sfatex@gmail.com>
2022-07-12 13:33:40 -0400

    Adjust button on submit functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss"
 export default function Contact() {
+  const [Message,setMessage] = useState(false)
+  const handleSubmit = (e)=>{
+    e.preventDefauult();
+    setMessage(true)
+  }
   return (
     <div className='contact' id="contact">
       <div className="left">
@@ -8,10 +13,11 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form>
+        <form >
           <input type="text" placeholder='Email' />
           <textarea placeholder='Message'></textarea>
           <button type='submit'>Send</button>
+          {message &&<span>Thanks for contacting me!Will reply soon</span>}
         </form>
       </div>
     </div>
