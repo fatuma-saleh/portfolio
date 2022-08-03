82460aeca9bc783b128cb3e368b47ab789a4012f
Fatuma <sfatex@gmail.com>
2022-08-03 14:10:32 -0400

    add project github link

--- src/data.js
+++ src/data.js
@@ -5,28 +5,32 @@ const data = [
     icon: "docs/add-tweet.png",
     title: "Tweeter",
     desc: "Tweeter is a simple single-page,AJAX-based Twitter clone that uses jQuery,HTML5 and plain CSS .",
-    img: "docs/add-tweet.png"
+    img: "docs/add-tweet.png",
+    project:"https://github.com/fatuma-saleh/tweeter"
   },
   {
     id: "2",
     icon: "docs/iCard.png",
     title: "iCard",
     desc: "iCard is a single-page application (SPA) that allows users to create digital business cards, and share it to others through an auto generated QR code. The front end of this project is built with React and makes requests to an API to fetch and store useer's data from a database.",
-    img: "docs/iCard.png"
+    img: "docs/iCard.png",
+    Project:"https://adoring-volhard-7e0798.netlify.app/"
   },
   {
     id: "3",
     icon: "docs/my_Urls.png",
     title: "TinyApp",
     desc: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs.The purpose of a URL shortener is to make a URL more manageable and shareable.",
-    img: "docs/my_Urls.png"
+    img: "docs/my_Urls.png",
+    Project:"https://github.com/fatuma-saleh/tinyapp"
   },
   {
     id: "4",
     icon: "docs/quizApp.png",
     title: "quizApp",
     desc: "quiz-app is an app that lets you create,take and share quizzes.The creator of the quiz can view and share all the results at the end of the quiz.",
-    img: "docs/quizApp.png"
+    img: "docs/quizApp.png",
+    Project:"https://github.com/fatuma-saleh/quiz-app"
   },
 ];
 module.exports = data;
