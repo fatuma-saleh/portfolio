bf5ea20bcad99e57d4eaa08cfe0500eac4186a90
Fatuma <sfatex@gmail.com>
2022-08-04 23:29:14 -0400

    Insert about me content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,24 @@ export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <h1>About</h1>
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <div className="aboutme">
+          <p>
+            &nbsp;&nbsp;&nbsp;My name is Fatuma. Currently, I am looking for a Junior Full Stack
+            Developer role. My communication skills developed as a manager and
+            business owner led to software development and now I enjoy creating
+            applications that connect the interface with the user. My strength
+            is my ability to truly understand what the end-user wants and work
+            closely with the product team to create a delightful user
+            experience. I am hardworking and eager to learn. My goal is to be a
+            Lead Full Stack Developer.What I am looking for is companies where I
+            can add value to and join a strong team that delivers core
+            customer-facing features.
+          </p>
+        </div>
+      </div>
+
+      <div className="right">rightkkkk</div>
     </div>
   );
 }
