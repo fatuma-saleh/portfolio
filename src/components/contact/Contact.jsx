e46111efc81c30da4a7308cab331b409b07711bd
Fatuma <sfatex@gmail.com>
2022-09-27 20:51:59 -0400

    Add full name in the contacts section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,6 +16,7 @@ export default function Contact() {
       <div className="right">
         <h2>Contact</h2>
         <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Full Name' />
           <input type="text" placeholder='Email' />
           <textarea placeholder='Message'></textarea>
           <button type='submit'>Send</button>
