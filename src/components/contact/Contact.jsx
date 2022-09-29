965eaf5b4c1d6455e66f874652421d9be79994a4
Fatuma <sfatex@gmail.com>
2022-09-28 23:45:37 -0400

    Add function to handlesubmit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,6 +7,12 @@ export default function Contact() {
   const handleSubmit = (e)=>{
     e.preventDefault();
     setMessage(true)
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
   }
   return (
     <div className='contact' id="contact">
