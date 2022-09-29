d8a2814c1de284df9538118fbf85498c2d10a6c0
Fatuma <sfatex@gmail.com>
2022-09-29 00:31:09 -0400

    Adjust input names to match emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,12 +4,14 @@ import HandshakeIcon from '@mui/icons-material/Handshake';
 import emailjs from '@emailjs/browser';
 export default function Contact() {
   const [message,setMessage] = useState(false)
+  const form = useRef();
   const handleSubmit = (e)=>{
     e.preventDefault();
     setMessage(true)
     emailjs.sendForm('service_tmjblio', 'template_0z0ei5f', form.current, 'p_TaevDwCZI_7UZBB')
     .then((result) => {
         console.log(result.text);
+        console.log("Message Sent")
     }, (error) => {
         console.log(error.text);
     });
@@ -23,8 +25,8 @@ export default function Contact() {
       <div className="right">
         <h2>Contact</h2>
         <form ref={form} onSubmit={handleSubmit}>
-        <input type="text" placeholder='Full Name' name="user_name" />
-          <input type="text" placeholder='Email'name="user_email" />
+        <input type="text" placeholder='Full Name' name="from_name" />
+          <input type="text" placeholder='Email'name="from_name" />
           <textarea placeholder='Message' name="message"></textarea>
           <button type='submit' value="send">Send</button>
           {message &&<span>Thanks for contacting me!Will reply soon!</span>}
